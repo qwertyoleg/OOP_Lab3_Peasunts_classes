a6265e42a6b6851ce86ff0e60575014a0b383293
Oleg Petrushov <olegpetrushov@icloud.com>
2016-05-26 10:40:03 +0300

    Initial Commit

---
+++ Lab 3 2016 Summer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab 3 2016 Summer
+//
+//  Created by Oleg Petrushov on 5/26/16.
+//  Copyright © 2016 XcoderUA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
